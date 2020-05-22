b0a00e9d662369cbd6e715451b5d02fd45e2ae25
Coman Alexandru <alexandru.coman@pitechplus.com>
2020-05-22 12:44:47 +0300

    Updated mongoDB connection pass

--- config/initializers/dbConnection/mongoDB.js
+++ config/initializers/dbConnection/mongoDB.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 module.exports = callback => {
     const connectionURL = 'mongodb+srv://Coman907014:9299swod5c@node-js-workshop-2020-bwtze.mongodb.net/test?retryWrites=true&w=majority';
     mongoose.connect(connectionURL, {
+        useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,

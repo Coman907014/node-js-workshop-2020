c5b2d0214eb7457fd9ea82bd8ed36d44adfa6727
Coman Alexandru <alexandru.coman@pitechplus.com>
2020-05-22 12:07:54 +0300

    Updated mongoDB connection URL

--- config/initializers/dbConnection/mongoDB.js
+++ config/initializers/dbConnection/mongoDB.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 module.exports = callback => {
-    const connectionURL = 'mongodb://task-api-database:27017/task-API';
+    const connectionURL = 'mongodb+srv://Coman907014:<password>@real-estate-db-bwtze.mongodb.net/test?retryWrites=true&w=majority';
     mongoose.connect(connectionURL, {
         useNewUrlParser: true,
         useCreateIndex: true,

c0ed11324a80d882c647b60606510ac07ae47a5b
Coman Alexandru <alexandru.coman@pitechplus.com>
2020-06-02 20:14:31 +0300

    Removed redundant elements

--- config/initializers/server.js
+++ config/initializers/server.js
@@ -22,10 +22,6 @@ module.exports = callback => {
   // server.listen(nconf.get('NODE_PORT'), () => {
   //   logger.info('Server listening on port: ' + nconf.get('NODE_PORT'));
   // });
-<<<<<<< HEAD
-
-=======
->>>>>>> Uncommented graphQL server
 
   callback();
 };

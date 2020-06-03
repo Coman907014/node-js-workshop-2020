b2d84dcf961fb9ac67c40acb6ae9e14092690348
Coman Alexandru <alexandru.coman@pitechplus.com>
2020-06-03 14:48:29 +0300

    Connected to cloud mongoDB

--- config/initializers/dbConnection/mongoDB.js
+++ config/initializers/dbConnection/mongoDB.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 module.exports = callback => {
-    const connectionURL = 'mongodb://task-api-database:27017/task-API';
+    const connectionURL = 'mongodb+srv://Coman907014:9299swod5c@node-js-workshop-2020-bwtze.mongodb.net/test?retryWrites=true&w=majority';
     mongoose.connect(connectionURL, {
         useNewUrlParser: true,
         useCreateIndex: true,
